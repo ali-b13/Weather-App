5af6ff1bb085976b3645208f3c32713f6f5d448e
ali <aliiskran004@gmail.com>
2022-12-08 23:52:14 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     hello
+     helloecho "# Weather-App" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ali-b13/Weather-App.git
+git push -u origin main
+     echo "# Weather-App" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ali-b13/Weather-App.git
+git push -u origin main
     </div>
   );
 }
