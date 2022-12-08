2f76b12788655ef2f8e9047a0a1f6ad4f334c908
ali <aliiskran004@gmail.com>
2022-12-08 23:53:43 +0530

    changed App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     helloecho "# Weather-App" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/ali-b13/Weather-App.git
-git push -u origin main
-     echo "# Weather-App" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/ali-b13/Weather-App.git
-git push -u origin main
+  Ali
     </div>
   );
 }
